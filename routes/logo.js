b4c9197e3ff89c6664f80425b5fe2b38c4d1713c
Seth Van Niekerk <seth@swvn.io>
2026-03-09 14:30:50 -0400

    Fix error status logging, team-not-found available teams, and deduplicate helpers

--- routes/logo.js
+++ routes/logo.js
@@ -12,6 +12,7 @@ const { downloadImage, buildSkipLogosTeam, resolveTeamsWithFallback, handleTeamN
 const { sendCachedOrGenerate, handleImageRouteError } = require('../helpers/routeUtils');
 const { getCachedImage, addToCache } = require('../helpers/imageCache');
 const { findLeague } = require('../leagues');
+const { getTeamDisplayName } = require('../helpers/teamUtils');
 const logger = require('../helpers/logger');
 
 module.exports = {
